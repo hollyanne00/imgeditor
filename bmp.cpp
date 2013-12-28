c16bcafb566917f4f9d067beb167f6dd37839be5
Holly <hollyanne00@googlemail.com>
2013-12-28 14:38:51 +0000

    class to hold bmp information

--- bmp.cpp
+++ bmp.cpp
@@ -27,7 +27,7 @@ void Bmp::saveCopy(std::string filename){
 // Get pixel values at position i, j
 // Note that pixel [0,0] is at [0,h-1]
 // Red
-const short int Bmp::getPixelR(int i, int j){
+const short int Bmp::getPixelR(int x, int y){
 
 	// Red is the third byte value
 	return (unsigned short int)((pixels.at(this->height-1-y).at((x*3)+2))&255);
@@ -35,7 +35,7 @@ const short int Bmp::getPixelR(int i, int j){
 }
 
 // Green
-const short int Bmp::getPixelG(int i, int j){
+const short int Bmp::getPixelG(int x, int y){
 
 	// Green is the second byte value
 	return (unsigned short int)((pixels.at(this->height-1-y).at((x*3)+1))&255);
@@ -43,9 +43,18 @@ const short int Bmp::getPixelG(int i, int j){
 }
 
 // Blue
-const short int Bmp::getPixelB(int i, int j){
+const short int Bmp::getPixelB(int x, int y){
 
 	// Blue is the first byte value
 	return (unsigned short int)((pixels.at(this->height-1-y).at(x*3))&255);
+
+}
+
+// Set R, G, B values of a pixel
+void Bmp::setPixel(int x, int y, short int r, short int g, short int b){
+
+	pixels[this->height-1-y][(x*3)+2] = (char) r;
+	pixels[this->height-1-y][(x*3)+1] = (char) g;
+	pixels[this->height-1-y][(x*3)] = (char) b;
 
 }
